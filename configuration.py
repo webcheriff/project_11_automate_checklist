dfdf4fcb07b91deeb7100ac2b5ad487cf6beca43
webcheriff <cher.rost@gmail.com>
2023-08-16 21:55:44 +0300

    Create files for project 11

---
+++ configuration.py
@@ -0,0 +1,4 @@
+URL_SERVICE = "https://66d8d68e-1903-4be7-9d66-6f9d8e4c0644.serverhub.praktikum-services.ru"
+DOC_PATH = "/docs/"
+LOG_MAIN_PATH = "/api/logs/main/"
+CREATE_PRODUCTS_KIT_PATH = "/api/v1/kits"
