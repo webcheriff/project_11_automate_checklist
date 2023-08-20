56a5d9350a8b0c7fab11d383e377824bb6872ac3
webcheriff <cher.rost@gmail.com>
2023-08-20 14:06:43 +0300

    1. Created a new function to get an authToken for a user. 2. Fixed a function of creating a kit.

--- sender_stand_request.py
+++ sender_stand_request.py
@@ -11,17 +11,15 @@ def post_new_user(user_body):
 	                     headers = data.headers)
 
 
-response = post_new_user(data.user_body)
-#   Getting an authToken from an answer and output it
-auth_token = response.json().get("authToken")
+#   Getting an authToken for a user
+def get_new_user_token():
+	response = post_new_user(data.user_body)
+	return response.json().get("authToken")
 
 
 #   Creating a kit
-def post_new_client_kit(kit_body, auth_token):
-	data.headers["Authorization"] += auth_token
+def post_new_client_kit(kit_body):
+	data.headers["Authorization"] += get_new_user_token()
 	return requests.post(configuration.URL_SERVICE + configuration.CREATE_PRODUCTS_KIT_PATH,
 	                     json = kit_body,
 	                     headers = data.headers)
-
-
-response = post_new_client_kit(data.kit_body, auth_token)
